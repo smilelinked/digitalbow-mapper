601deaa050c37bc7ce04a94507e57fa6f380f6cc
lmxia <xialingming@gmail.com>
2023-07-26 10:52:59 +0800

    add new lib
    Signed-off-by: lmxia <xialingming@gmail.com>

--- driver/client.go
+++ driver/client.go
@@ -19,9 +19,9 @@ package driver
 //#cgo CFLAGS: -I./number
 //#cgo LDFLAGS: -L${SRCDIR}/number -lSixDof -lm
 //
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include<math.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
 //#include "s_6dof.h"
 import "C"
 import (
